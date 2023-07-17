37a7d32f06830ea2daeba9cf594e1074aa0fa82a
Mark <marikgames12@gmail.com>
2023-07-17 18:34:01 +0300

    Added: - Changed the font when entering into the field - Added a field

--- src/ListComponents.java
+++ src/ListComponents.java
@@ -1,7 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ListComponents extends JFrame {
+    private JLabel myList;
+    private JTextField field;
     public ListComponents(){
         super("List of tasks");
+        setLayout(null);
+
+        myList = new JLabel("Add tasks!");
+        myList.setBounds(220, -30, 100, 100);
+
+        Font font = new Font("Arial", Font.BOLD, 18);
+        myList.setFont(font);
+
+        field = new JTextField();
+        field.setBounds(115, 50, 320,60);
+        field.setFont(font);
+
+        add(myList);
+        add(field);
     }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class MainWindow {
     public static void main(String[] args) {
        ListComponents listWindow = new ListComponents();
-        listWindow.setSize(560, 700);
+       listWindow.setSize(560, 700);
        listWindow.setLocationRelativeTo(null);
        listWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
        listWindow.setVisible(true);
