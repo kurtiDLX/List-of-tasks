b4573dae17c3a1b37cabc93edd340db5ca677da8
Mark <marikgames12@gmail.com>
2023-07-17 18:12:10 +0300

    first commit

---
+++ src/ListComponents.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class ListComponents extends JFrame {
+    public ListComponents(){
+        super("List of tasks");
+    }
+}
---
+++ src/MainWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainWindow {
+    public static void main(String[] args) {
+       ListComponents listWindow = new ListComponents();
+        listWindow.setSize(560, 700);
+       listWindow.setLocationRelativeTo(null);
+       listWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       listWindow.setVisible(true);
+    }
+}
